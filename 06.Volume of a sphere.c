b8fd6f2e625b481e176b909fdf05cecf6cb17a69
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-02 21:59:38 +0600

    Update 06.Volume of a sphere.c

--- 06.Volume of a sphere.c
+++ 06.Volume of a sphere.c
@@ -14,14 +14,6 @@ int main()
     return 0;
 }
 
-/*  *****Notes*****
-    div_result=(4/3) * pi * R*R*R; 
-    It's not work
-    In C programming, when you perform the division operation using integers, the result will be an integer. In the line of code you mentioned, `(4/3)` will result in `1` because both `4` and `3` are integers.
-
-    To fix this issue, you can change either `4` or `3` to a float value. For example, you can write `(4.0/3)` or `(4/3.0)` to ensure that the division operation produces a float result.
-    */
-
 /*
 input: R = 5
 output: VOLUME = 523.598
@@ -29,3 +21,11 @@ output: VOLUME = 523.598
 input: R = 9
 output: VOLUME = 3053.626
 */
+
+/*  *****Notes*****
+    div_result=(4/3) * pi * R*R*R; 
+    It's not work
+    In C programming, when you perform the division operation using integers, the result will be an integer. In the line of code you mentioned, `(4/3)` will result in `1` because both `4` and `3` are integers.
+
+    To fix this issue, you can change either `4` or `3` to a float value. For example, you can write `(4.0/3)` or `(4/3.0)` to ensure that the division operation produces a float result.
+    */
