70d715acde7de4f58b961214018b152e44592b67
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-03 23:57:34 +0600

    Add files via upload

---
+++ 13.Addition one Integer and Float.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+   int a;
+
+   float b;
+   float sum=a+b;
+
+   scanf("%d %f",&a,&b);
+
+   printf("a + b = %d + %.2f = %.2f\n",a,b,a+b);
+
+
+
+    return 0;
+}
+
+/*
+input: a=7, b=9.5
+output:a + b = 7 + 9.50 = 16.50
+*/
+
+
