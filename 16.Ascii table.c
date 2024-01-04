d2dd3601676cb933b2f917b6d2d4f352efe20ebe
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-04 20:21:06 +0600

    Add files via upload

---
+++ 16.Ascii table.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    printf("%c\n",97);
+    printf("%c\n",98);
+    printf("%c\n",99);
+    printf("%c\n",100);
+    printf("%c\n",101);
+    printf("%c\n",102);
+    printf("%c\n",103);
+    printf("%c\n",104);
+    printf("%c\n",105);
+    printf("%c\n",106);
+    printf("%c\n",107);
+    printf("%c\n",108);
+    printf("%c\n",109);
+    printf("%c\n",110);
+    printf("%c\n",111);
+    printf("%c\n",112);
+    printf("%c\n",113);
+    printf("%c\n",114);
+    printf("%c\n",115);
+    printf("%c\n",116);
+    printf("%c\n",117);
+    printf("%c\n",118);
+    printf("%c\n",119);
+    printf("%c\n",120);
+    printf("%c\n",121);
+    printf("%c\n",122);
+    return 0;
+}
