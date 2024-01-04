c52bfaf9ad18fa3131882da43652097d8f3c55f7
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-04 20:00:56 +0600

    Add files via upload

---
+++ 15.All data type.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+
+{
+   int age = 25;
+   float price = 19.99;
+   double pi = 3.141592;
+   char initial = 'A';
+
+
+   printf("The value of int %d\n",age);
+   printf("The value of float %.2f\n",price);
+   printf("The value of double %lf\n",pi);
+   printf("The value of char %c\n",initial);
+
+return 0;
+
+}
