f050cb0e848f4a875b3885914a6cbafadee99e04
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-04 23:55:09 +0600

    Add files via upload

---
+++ 20.Average of number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+     float A,B,C;
+     float avg;
+     scanf("%f %f %f",&A,&B,&C);
+     avg=(A+B+C)/3.0;
+
+     printf("Average  = %.2f\n",avg);
+
+    return 0;
+}
+
+/*
+output: 33 55 77
+        Average  = 55.00
+*/
+
