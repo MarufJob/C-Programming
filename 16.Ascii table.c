b996a032dbf5206923e3953bac4c4ea4070dff3d
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-04 21:40:34 +0600

    Update 16.Ascii table.c

--- 16.Ascii table.c
+++ 16.Ascii table.c
@@ -29,3 +29,33 @@ int main()
     printf("%c\n",122);
     return 0;
 }
+
+
+/*
+output: a
+        b
+        c
+        d
+        e
+        f
+        g
+        h
+        i
+        j
+        k
+        l
+        m
+        n
+        o
+        p
+        q
+        r
+        s
+        t
+        u
+        v
+        w
+        x
+        y
+        z
+*/
