28a2b0a14f15cff6ce0460ffdd592c6555bbbcee
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-06 23:48:13 +0600

    Add files via upload

---
+++ 30.EOF file termination Part-01.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+
+{
+    int a,b,sum;
+
+    while (scanf("%d %d",&a,&b)!=EOF)
+
+    {
+    sum=a+b;
+
+    printf("%d\n",sum);
+
+
+    }
+
+
+    return 0;
+}
+//EOF will be used where there inputting process is closed.
+//Advanced loop for programming contest
+
+
+/*
+Input: 45 78
+Output:123
+
+Input:23 57
+Output:80
+
+Input:56 89
+Output:145
+*/
