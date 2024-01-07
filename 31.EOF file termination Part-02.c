8ea5eabbba5add4aca4981836d2df14c2db96b84
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-07 22:43:13 +0600

    Update 31.EOF file termination Part-02.c

--- 31.EOF file termination Part-02.c
+++ 31.EOF file termination Part-02.c
@@ -2,7 +2,7 @@
 int main()
 
 {
-    int a,b,sum,n,i;
+    int a, sum,n,i;
 
     while(scanf("%d",&n)!=EOF){
     sum=0;
@@ -19,3 +19,18 @@ int main()
 }
 //EOF will be used where there inputting process is closed.
 //Advanced loop for programming contest
+/*
+Input:2     4
+            4
+Output:Sum = 8
+Input:3     2
+            3
+            4
+Output:Sum = 9
+Input:5     1
+            2
+            3
+            4
+            5
+Output:Sum = 15
+*/
