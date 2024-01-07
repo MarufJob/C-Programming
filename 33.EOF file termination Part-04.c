c2d32e59b25c52aaebc5661a1662b1ae6960986a
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-07 23:07:40 +0600

    Add files via upload

---
+++ 33.EOF file termination Part-04.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+
+{
+    int a,sum,n,i,T;
+
+    scanf("%d",&T);
+
+    while(T--){
+    scanf("%d",&n);
+    sum=0;
+    for(i=1; i<=n; i++)
+        {
+        scanf("%d",&a);
+        sum=sum+a;
+
+        }
+    printf("sum = %d",sum);
+
+    }
+
+
+    return 0;
+}
+//In this program user will input his  according his own way
+//Advanced loop for programming contest
+
+/*
+Input:2      1
+             2
+Output:sum = 2
+
+Input:3       2
+              4
+              5
+Output:sum = 11
+*/
+
