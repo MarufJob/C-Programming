715dc53a3a171a0abcc3c9553ff0fe99ee4b8c39
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-08 23:47:08 +0600

    Add files via upload

---
+++ 40.Find Data type sizeof.c
@@ -0,0 +1,23 @@
+//bool -> char -> int -> float -> double
+
+#include <stdio.h>
+#include <stdbool.h> //bool' is defined in header '<stdbool.h>'
+
+int main()
+
+{
+
+    printf("bool size %d\n",sizeof(bool));
+    
+    printf("char size %d\n",sizeof(char));
+    
+    printf("int size %d\n",sizeof(int));
+    
+    printf("float size %d\n",sizeof(float));
+    
+    printf("double size %d\n",sizeof(double));
+
+    return 0;
+}
+
+
