2e927f22ca3c4ef484628668ae66bf83aa54b5c6
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-09 23:06:20 +0600

    Add files via upload

---
+++ 44.Logical Operator(Not).c
@@ -0,0 +1,19 @@
+// && || !
+// && Logical and
+// || Logical or
+// ! not
+
+#include <stdio.h>
+int main()
+{
+    int math;
+    int check;
+    math=55;
+     check= !(math==6);
+     printf("%d",check);
+
+    return 0;
+}
+/*
+Output:1
+*/
