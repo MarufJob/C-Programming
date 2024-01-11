39e131ed410f05c820daf8e9f05bccf98c62f571
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-11 23:19:39 +0600

    Add files via upload

---
+++ 52.Loop(Advanced)How many numbers sum.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sum,n,i;
+
+    scanf("%d",&n);
+    sum=0;
+
+    for(i=1; i<=n; i++)
+    {
+    scanf("%d",&a);
+    sum=sum+a;
+    }
+   printf("%d",sum);
+
+
+    return 0;
+}
+
+/*
+Input:  3
+        4 5 6
+Output:15
+
+Input:  5
+        9 7 5 3 1
+Output:25
+
+Input:  4
+        1 2 3 4
+Output:10
+*/
