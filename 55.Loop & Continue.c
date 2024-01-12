326bdd5ae2ee136e4bea4d190c17366f65f6fc92
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-12 22:04:41 +0600

    Add files via upload

---
+++ 55.Loop & Continue.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+        int i;
+
+        for (i=0; i<5; i++)
+
+        {
+             printf("%d ",i);
+
+            if (i==3)
+            continue;
+
+            printf("%d ",i);
+
+
+        }
+// For using using continue statement every value will be printed two times except 3 in this program
+            return 0;
+}
+
+/*
+Output:0 0 1 1 2 2 3 4 4 
+*/
+
+
